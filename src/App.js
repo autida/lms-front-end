6f7acd6abbd721db67737fd6c533974b58c53130
autida <autidaanalita@gmail.com>
2023-06-26 16:53:13 +0800

    Updated index.js and App.js file for routing purposes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import './App.css';
 import React, { useState } from 'react';
 import Header from './Header';
-import Login from './login';
+import Login from './pages/login';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
-  return(
-    <div className='App'>
-      <Header />
-      {/* <Router> */}
-        {/* <Switch> */}
-          {/* <Route exact path="/" component={Home} /> */}
-          {/* <Route path="/login" component={Login} /> */}
-        {/* </Switch> */}
-      {/* </Router> */}
-    </div>
-  )
+  return <Routes>
+    <Route />
+
+    <Route />
+  </Routes>
+    // <div className='App'>
+    //   <Header />
+      
+    // </div>
+  
 }
 
 export default App;
